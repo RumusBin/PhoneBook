a862949e8ee92cc5498156c0fd4d36889811b3c0
RumusBin <komlikov.r@gail.com>
2017-11-04 18:33:37 +0200

    start project. Create main view, header and footer components

---
+++ resources/assets/js/components/Myfooter.vue
@@ -0,0 +1,12 @@
+<template>
+    <footer class="footer">
+        <div class="container">
+            <div class="content has-text-centered">
+                <p>
+                    <strong>Phone Book App</strong> by <a href="https://github.com/RumusBin/phonebook">RumusBin</a>. The source code is licensed
+                    RumiusBin.
+                </p>
+            </div>
+        </div>
+    </footer>
+</template>
